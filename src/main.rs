f8f082fa86a7f1bb60d918585263c263fe95ecd2
gb10udn <rbbpx0926@gmail.com>
2024-07-22 19:33:51 +0900

    remove function to add module name to

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     }
 
     write_each_code(path);
-    write_summary_code(path, "Utils", true, true);  // TODO: 240722 with_module_name ... は紛らわしいので、削除を検討せよ。
+    write_summary_code(path, "Utils", true);
     stop();
 }
 
@@ -34,7 +34,7 @@ fn write_each_code(path: &str) {  // TODO: 240128 エクセルファイル名で
 }
 
 /// 複数のモジュールを結合して、１つの .bas として保存する関数。
-fn write_summary_code(path: &str, dst_module_name: &str, remove_test_code: bool, with_module_name: bool) {  // FIXME: 240128 Utils.bas の名称が衝突する場合のエラー処理を書くこと。 (panic! で強制的にストップさせてよいかも？)
+fn write_summary_code(path: &str, dst_module_name: &str, remove_test_code: bool) {  // FIXME: 240128 Utils.bas の名称が衝突する場合のエラー処理を書くこと。 (panic! で強制的にストップさせてよいかも？)
     let mut workbook: Xlsx<_> = open_workbook(path).expect("Cannot open file");
     
     if let Some(Ok(mut vba)) = workbook.vba_project() {
@@ -57,11 +57,7 @@ fn write_summary_code(path: &str, dst_module_name: &str, remove_test_code: bool,
                 }
                 
                 if (is_test_block == false) && (one_line.starts_with("Option Explicit") == false) && (one_line.starts_with("Attribute ") == false) {
-                    if with_module_name {
-                        summary_vba_code.push_str(&format!("{}\n", add_module_name(one_line, module_name)));
-                    } else {
-                        summary_vba_code.push_str(&format!("{}\n", one_line));
-                    }
+                    summary_vba_code.push_str(&format!("{}\n", one_line));
                 }
 
                 if (remove_test_code == true) && (is_test_block == true) && (re_test_block_end.is_match(one_line) == true) {
@@ -77,18 +73,6 @@ fn write_text(text: &str, dst: &str) -> Result<(), Box<dyn std::error::Error>> {
     let mut file = File::create(dst)?;
     write!(file, "{}", text)?;
     Ok(())
-}
-
-/// Sub or Function の名前の先頭にモジュール名を付与する関数
-/// Ex. Sub Hoge() --> Sub Module_Hoge() (Hoge というサブプロシージャの先頭に、モジュール名の Module を追加して返した。)
-fn add_module_name(arg: &str, module_name: &str) -> String {
-    for target_reserved_word in vec!["Public Sub", "Public Function"] {
-        if arg.contains(target_reserved_word) == true && arg.contains("End") == false {
-            let word_vec: Vec<&str> = arg.split(target_reserved_word).collect();
-            return format!("{}{} {}_{}", word_vec[0], target_reserved_word, module_name, word_vec[1].trim());
-        }
-    }
-    return String::from(arg);
 }
 
 fn stop() {
